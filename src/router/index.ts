7ccca3dc4e1775cb95a882aeb1bad167f78b950f
ParmezhovSergey <parmezhovsergey@mail.ru>
2025-06-18 14:13:40 +0300

    7

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import PageUsers from '@/views/PageUsers.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import PageUsers from './../views/PageUsers.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
